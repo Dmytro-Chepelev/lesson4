b5d7b7c1ff9a1898d489549d109d92f111995e97
Dmytro Chepelev <dmytro.chepelev@gmail.com>
2022-04-07 06:13:30 +0300

    modify QuadraticTest.java testWithTwoSolutions() for compare roots in any order

--- src/test/java/net/absoft/equation/QuadraticTest.java
+++ src/test/java/net/absoft/equation/QuadraticTest.java
@@ -17,20 +17,28 @@ public class QuadraticTest {
 
         Assert.assertEquals(solution.getFirstSolution(), x, "unexpected value of first solution");
         Assert.assertEquals(solution.getSecondSolution(), x,"second solution is not equal first");
+
+        Assert.assertEquals(solution.x1, x, "unexpected value of first solution");
+        Assert.assertEquals(solution.x2, x,"second solution is not equal first");
+
     }
 
     @Test
     public void testWithTwoSolutions () {
         long a = 1, b = -4, c = -5;
-        double x1 = -1.0, x2 = 5.0;  // from min to max
+        double x1 = -1.0, x2 = 5.0;
+        OrderedPare expectedValue = new OrderedPare(x1, x2);
 
         Quadratic quadratic = new Quadratic ();
         Solution solution = quadratic.getSolution(a, b, c);
 
         Assert.assertNotNull(solution, "getSolution() can't compute result");
 
-        Assert.assertEquals(x1, solution.getFirstSolution(), "unexpected value of first solution");
-        Assert.assertEquals(x2, solution.getSecondSolution(), "unexpected value of second solution");
+        OrderedPare byFunctionValue = new OrderedPare(solution.getFirstSolution(), solution.getSecondSolution());
+        OrderedPare byFieldValue = new OrderedPare(solution.x1, solution.x2);
+
+        Assert.assertTrue (byFunctionValue.isEqual(expectedValue), "expected and computed solution (get by getter) is not equal");
+        Assert.assertTrue (byFieldValue.isEqual(expectedValue), "expected and computed solution (get by field) is not equal");
     }
 
     @Test
